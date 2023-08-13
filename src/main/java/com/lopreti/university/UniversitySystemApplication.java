cd85f465ea19bb77c3061fccef2f629ea96e793b
Rafael Lopreti <rafael.lopreti@gmail.com>
2023-08-13 19:57:59 -0300

    UNI-000 Hexagonal Structure

---
+++ src/main/java/com/lopreti/university/UniversitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.lopreti.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopreti/university/UniversitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopreti.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
