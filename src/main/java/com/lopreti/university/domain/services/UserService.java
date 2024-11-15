5e1c77ed2e8f2764c7394816df19f9b2d4a69245
Allan Lopreti <allanlopreti@MacBook-de-Allan.local>
2024-11-15 07:47:19 -0300

    UNI-027 Modify regex password and tests

--- src/main/java/com/lopreti/university/domain/services/UserService.java
+++ src/main/java/com/lopreti/university/domain/services/UserService.java
@@ -29,8 +29,8 @@ public class UserService {
             Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
                     + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", Pattern.CASE_INSENSITIVE);
 
-    private static final Pattern VALID_PASSWORD_REGEX =
-            Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=()\\-])(?=\\S+$).{8,20}$");
+    public static final Pattern VALID_PASSWORD_REGEX =
+            Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=()\\-_*])(?=\\S+$).{8,20}$");
 
     public UserService(UsersRepositoryImpl usersRepository) {
         this.usersRepository = usersRepository;
